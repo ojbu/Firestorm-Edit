90a820dff40a9bfdc8fb37f178eeba7caa624b32
TommyTheTerrible <81168766+TommyTheTerrible@users.noreply.github.com>
2024-07-29 02:31:54 -0400

    Texture Delete Timeouts and Threshold Reduced
    Delete timeouts set to a maximum of 60 seconds, down from 2 minutes.
    
    And instant delete is set to 80% of VRAM used.

--- indra/newview/llviewertexturelist.cpp
+++ indra/newview/llviewertexturelist.cpp
@@ -1025,9 +1025,9 @@ void LLViewerTextureList::updateImageDecodePriority(LLViewerFetchedTexture* imag
 
     //imagep->setDebugText(llformat("%.3f - %d", sqrtf(imagep->getMaxVirtualSize()), imagep->getBoostLevel()));
 
-    F32 lazy_flush_timeout = 60.f;   // Delete after n seconds, or 0 to not delete until VRAM threshold reached.
-    F32 max_inactive_time = 60.f;   // Stop making changes to texture after n seconds.
-    F32 vram_max_percent = 0.95f;  // If vram reaches this percentage, we consider it full.
+    F32 lazy_flush_timeout = 30.f;   // Delete after n seconds, or 0 to not delete until VRAM threshold reached.
+    F32 max_inactive_time = 30.f;   // Stop making changes to texture after n seconds.
+    F32 vram_max_percent = 0.80f;  // If vram reaches this percentage, we consider it full.
     U32 current_free_vram = gViewerWindow->getWindow()->getAvailableVRAMMegabytes();
     bool vram_full = (current_free_vram < gGLManager.mVRAMDetected * (1 - vram_max_percent));
     S32 min_refs = 3; // 1 for mImageList, 1 for mUUIDMap, 1 for local reference
